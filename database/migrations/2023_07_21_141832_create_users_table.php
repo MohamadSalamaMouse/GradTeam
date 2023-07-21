d52f89ec0dc7598c690a53bdfdfeedaee376512c
EmanElhelaly <emanelhelaly11@gmail.com>
2023-07-21 16:29:18 +0200

    first commit

---
+++ database/migrations/2023_07_21_141832_create_users_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id')->unsigned()->index();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->string('password');
+            $table->string('track')->nullable();
+            $table->string('bio')->nullable();
+            $table->string('imageUrl')->nullable();
+            $table->string('githubUrl')->nullable();
+            $table->string('facebookUrl')->nullable();
+            $table->string('linkedinUrl')->nullable();
+            $table->boolean('isLeader')->default(false);
+            $table->boolean('isInTeam')->default(false);
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
